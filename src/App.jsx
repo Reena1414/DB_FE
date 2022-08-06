0cd3d98cc1221012ccd9ef93cefacca959575144
Reena1414 <reenachhonkar1213@gmail.com>
2022-08-06 16:44:42 +0530

    added dropdown

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { Dropdown } from "./components/Dashboard/Dropdown";
 import { Security } from "./components/SecurityController/Security";
 import { Trade } from "./components/TradeController/Trade";
 
 const App = () => {
   return <>
+       <Dropdown/>
        <Security />
        <Trade/>
         </>;
